5808be9adfb3192a2eb7a895d32a686f69d07584
Hazza203 <s3383212@student.rmit.edu.au>
2019-05-22 00:13:11 +0800

    Updates

---
+++ precache-manifest.93c80eb9f74acb1f3c51524c9355b80b.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "834f2464c5aac87ba5406693ed2b8a1d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "641afd03388efb3b03ce",
+    "url": "/static/css/2.89666679.chunk.css"
+  },
+  {
+    "revision": "3772267765ec13c9f75a",
+    "url": "/static/css/main.601e2ca3.chunk.css"
+  },
+  {
+    "revision": "641afd03388efb3b03ce",
+    "url": "/static/js/2.21062914.chunk.js"
+  },
+  {
+    "revision": "3772267765ec13c9f75a",
+    "url": "/static/js/main.b61c0437.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "735eaaa70c0facd15e547eed69b9144a",
+    "url": "/static/media/backgroundGradient.735eaaa7.png"
+  },
+  {
+    "revision": "88f7464dbeb429aa51b3a755064f0c6c",
+    "url": "/static/media/binance.88f7464d.jpg"
+  },
+  {
+    "revision": "c3611e73e844b7da42537efa242c511b",
+    "url": "/static/media/blockchain_path.c3611e73.jpg"
+  },
+  {
+    "revision": "cf04e207357795167ea597ea6cf2ade8",
+    "url": "/static/media/faceFinder.cf04e207.png"
+  },
+  {
+    "revision": "3e115ad4326233739f48aeb99efe42c0",
+    "url": "/static/media/faceFinder2.3e115ad4.png"
+  },
+  {
+    "revision": "237fcc4df6aa8938c6511cd9231e1d0f",
+    "url": "/static/media/faceFinder3.237fcc4d.jpeg"
+  },
+  {
+    "revision": "8d006a456bab42a71908d242c9d0b68f",
+    "url": "/static/media/macd.8d006a45.png"
+  },
+  {
+    "revision": "2adb4e53d22dc62fd3cbc2b04cee649a",
+    "url": "/static/media/macd2.2adb4e53.gif"
+  },
+  {
+    "revision": "2c6c380e3f7307a9e429a7bd011148f4",
+    "url": "/static/media/pacman.2c6c380e.jpg"
+  },
+  {
+    "revision": "7dc50de2b7f46cbc991c693c968b465e",
+    "url": "/static/media/profilepic-circle.7dc50de2.png"
+  },
+  {
+    "revision": "ea875326d6985530db1c15b58824d315",
+    "url": "/static/media/profilepic.ea875326.jpg"
+  },
+  {
+    "revision": "77df835c153f068be9ad334cccbcfc57",
+    "url": "/static/media/studcoin.77df835c.png"
+  },
+  {
+    "revision": "f845320a0492d220179d6863865a4179",
+    "url": "/static/media/studcoin2.f845320a.png"
+  }
+]);
