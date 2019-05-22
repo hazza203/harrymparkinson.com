f736cce7cea038c3fd3edf9af79656aaf8ccbf9a
Hazza203 <s3383212@student.rmit.edu.au>
2019-05-22 16:38:19 +0800

    Fixed resposive animation bug

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -51,14 +51,14 @@ class Nav extends React.Component {
 			<div className={width < 600 ? navStyles.smallMenu : navStyles.nav} style={this.props.fixed ? this.state.fixed : null}>
 			{
 				width < 600 ?
-					<nav id='navbar'>
+					<nav style={{width: '100%'}} id='navbar'>
 						<div className={navStyles.wrapper}>
 							<img className={navStyles.menuImg} src={menuImg} alt="Menu icon" onClick={this.onClick}/>
 						</div>	
 						
 							<div className={navStyles.hiddenMenu} style={this.state.showMenu} >
 								<ul>
-									<li className={navStyles.hiddenItem}> <Link activeClass={navStyles.active} to="homeSection" spy={true} smooth={true} offset={0} duration= {500} onClick={this.onClick}>Home</Link> </li>
+									<li className={navStyles.hiddenItem} style={{paddingTop: "25px"}}> <Link activeClass={navStyles.active} to="homeSection" spy={true} smooth={true} offset={0} duration= {500} onClick={this.onClick}>Home</Link> </li>
 									<li className={navStyles.hiddenItem}> <Link activeClass={navStyles.active} to="aboutSection" spy={true} smooth={true} offset={this.props.fixed ? 0 : -180} duration= {500} onClick={this.onClick}>About</Link> </li>
 									<li className={navStyles.hiddenItem}> <Link activeClass={navStyles.active} to="portfolioSection" spy={true} smooth={true} offset={this.props.fixed ? 0 : -180} duration= {500} onClick={this.onClick}>Portfolio</Link> </li>
 									<li className={navStyles.hiddenItem}> <Link activeClass={navStyles.active} to="contactSection" spy={true} smooth={true} offset={this.props.fixed ? 0 : -180} duration= {500} onClick={this.onClick}>Contact</Link> </li>
